b21cf634c0c6d76a263b213b819a8003cad9ab95
yunjinchoi <yunjinchoi@mobinoma.com>
2022-07-28 00:03:42 +0900

    Initial commit

---
+++ src/main/java/com/example/codingtest/july/first.java
@@ -0,0 +1,12 @@
+package com.example.codingtest.july;
+
+public class first {
+    public static void main(String[] args) {
+        Integer j=0;
+        for(int i=0; i<10; i++){
+            j += i;
+        }
+
+        System.out.println("GG");
+    }
+}
